86a60c9d3ce3cfcb64f9388b9f68ab337cf67d83
Deepalimane25 <manedeepali98@gmail.com>
2020-07-18 11:00:08 +0530

    Firebase config

--- pro-organiser-application/src/Firebase/config.js
+++ pro-organiser-application/src/Firebase/config.js
@@ -2,14 +2,15 @@ import firebase from 'firebase';
 import 'firebase/auth';
 
  // Your web app's Firebase configuration
- var firebaseConfig = {
-  apiKey: "AIzaSyDVuTLQET_rJFMdeJAQaYkA1hXZrUkYkNw",
-  authDomain: "pro-organizer-app-430d4.firebaseapp.com",
-  databaseURL: "https://pro-organizer-app-430d4.firebaseio.com",
-  projectId: "pro-organizer-app-430d4",
-  storageBucket: "pro-organizer-app-430d4.appspot.com",
-  messagingSenderId: "171390799095",
-  appId: "1:171390799095:web:8c9e5833b1b2bffcedd7f2"
+ const firebaseConfig = {
+  apiKey: "AIzaSyBoUEnqKqPhskN938j9iPngvQT0AWf0g-s",
+  authDomain: "pro-organiser-app-25.firebaseapp.com",
+  databaseURL: "https://pro-organiser-app-25.firebaseio.com",
+  projectId: "pro-organiser-app-25",
+  storageBucket: "pro-organiser-app-25.appspot.com",
+  messagingSenderId: "330105416521",
+  appId: "1:330105416521:web:ff0d97d8d82db367e4c898",
+  measurementId: "G-7YT5E1SZEK"
 };
 // Initialize Firebase
 export const firebaseApp = firebase.initializeApp(firebaseConfig);
